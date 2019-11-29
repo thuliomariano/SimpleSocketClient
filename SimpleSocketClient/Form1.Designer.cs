7e4fb62d55dc096075108b0b7a230289bf319e0b
Thulio José Mariano <thulio.mariano@gmail.com>
2019-11-29 12:12:40 -0300

    Inicializando projeto

---
+++ SimpleSocketClient/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SimpleSocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHistorico = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Chat = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtHistorico);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Chat);
+            this.groupBox1.Controls.Add(this.btnEnviar);
+            this.groupBox1.Controls.Add(this.txtMensagem);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 268);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Socket";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Encerrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Historico";
+            // 
+            // txtHistorico
+            // 
+            this.txtHistorico.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtHistorico.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtHistorico.Location = new System.Drawing.Point(6, 85);
+            this.txtHistorico.Multiline = true;
+            this.txtHistorico.Name = "txtHistorico";
+            this.txtHistorico.Size = new System.Drawing.Size(465, 177);
+            this.txtHistorico.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Chat
+            // 
+            this.Chat.AutoSize = true;
+            this.Chat.Location = new System.Drawing.Point(3, 15);
+            this.Chat.Name = "Chat";
+            this.Chat.Size = new System.Drawing.Size(29, 13);
+            this.Chat.TabIndex = 2;
+            this.Chat.Text = "Chat";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(249, 27);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(6, 29);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(237, 20);
+            this.txtMensagem.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 304);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHistorico;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Chat;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SimpleSocketClient/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SimpleSocketClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string mensagem;
+        public string historico;
+
+        //cria uma stancia para iniciar uma conexão do socket
+        Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //conecta ao ip no caso meu mesmo e uma porta
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7070);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+
+            //conecta com o servidor
+            sck.Connect(endPoint);
+
+            txtHistorico.Text = "Mensagem: Cliente conectado com o Servidor!";
+            
+
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            //envia informações para o servidor
+            mensagem = txtMensagem.Text;
+
+            //enviando minha menssagem
+            byte[] msgBuffer = Encoding.Default.GetBytes(mensagem);
+            sck.Send(msgBuffer, 0, msgBuffer.Length, 0);
+            txtHistorico.Text = "Mensagem cliente: enviado mensagem com sucesso...";
+
+            //recebendo minha menssagem
+            byte[] buffer = new byte[255];
+            int rec = sck.Receive(buffer, 0, buffer.Length, 0);
+
+            Array.Resize(ref buffer, rec);
+
+            txtHistorico.Text = Encoding.Default.GetString(buffer);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sck.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
